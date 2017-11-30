fb77737f7143354e5a438e2dab1c282706abaa25
liuyao <18252063065@163.com>
2017-11-30 23:25:24 +0800

    dd

--- src/main/java/com/ccq/membercenter/api/AdminUserInfoController.java
+++ src/main/java/com/ccq/membercenter/api/AdminUserInfoController.java
@@ -1,8 +1,12 @@
 package com.ccq.membercenter.api;
 
 import com.ccq.framework.lang.JsonResult;
+import com.ccq.framework.lang.Page;
 import com.ccq.framework.lang.Result;
+import com.ccq.membercenter.access.AccessLevel;
+import com.ccq.membercenter.model.AdminUserInfo;
 import com.ccq.membercenter.service.intf.AdminUserInfoService;
+import com.ccq.membercenter.token.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController("adminUserInfoController")
+@RequestMapping(value = "/manager/user")
 public class AdminUserInfoController {
 
     @Autowired
@@ -19,10 +24,34 @@ public class AdminUserInfoController {
     @RequestMapping(value = "/login")
     public JsonResult login(HttpServletRequest request, String username, String password) throws ServletException {
 
-        Result r = adminUserInfoService.userCert(username,password);
-        if(r.isSuccess()) {
-            request.getSession().setAttribute("username",username);
-            return JsonResult.getSuccessResult(r.getMessage());
-        }else return JsonResult.getErrorResult(r.getMessage());
+        Result r = adminUserInfoService.userCert(username, password);
+        if (r.isSuccess()) {
+            JsonResult jr = JsonResult.getSuccessResult("登录成功");
+            jr.setObject(r.getObject());
+            return jr;
+        } else return JsonResult.getErrorResult(r.getMessage());
+    }
+
+    /**
+     *
+     * 根据条件来查询status等条件来进行查询
+     *
+     * @param request
+     * @param status
+     * @param username
+     * @param accessLevel
+     * @param pageSize
+     * @param pageNum
+     * @return
+     */
+    @RequestMapping("/list")
+    public JsonResult getAdminuserInfoList(HttpServletRequest request,String status,
+                                           String username, int accessLevel, int pageSize, int pageNum) {
+
+        /* 根据分页信息构建分页对象 */
+        Page page =Page.getPage(pageNum,pageSize);
+
+        //return adminUserInfoService.getAdminUserList();
+        return null;
     }
 }
